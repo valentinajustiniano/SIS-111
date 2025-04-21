ed2acbae00fac8788fa28b2ec4af1e8d29a0a7ad
valentinajustiniano <valentina.justiniano.g@ucb.edu.bo>
2025-04-21 13:02:36 -0400

    Add files via upload

---
+++ operadores_comparacion.py
@@ -0,0 +1,33 @@
+
+#menor que 
+variable1 = 10
+variable2 = 12
+
+resultado = variable1 < variable2
+print(resultado)
+
+
+#mayor igual que 
+variable3 = 500
+variable4 = 200
+resp = variable3 >= variable4
+print("mayor igual que")
+print(resp)
+
+
+#menor igual que
+variable5 = 501
+variable6 = 500
+res = variable5 <= variable6
+print(res)
+
+
+#igual
+resultado_igual = 100 == 100.1
+print("igual")
+print(resultado_igual)
+
+
+#diferente / distinto 
+resultado_diferente = 500 != 501
+print(resultado_diferente)
---
+++ operadores_logicos.py
@@ -0,0 +1,19 @@
+#and 
+
+respuesta_and = 500 == 500 and 200 != 201 
+print(respuesta_and)
+
+respuesta_ands = 500 == 500 and 200 != 201 and 80 > 100
+print(respuesta_ands)
+
+
+#or 
+
+respuesta_or = 500 == 500 or 200 != 201 or 80 > 100
+print(respuesta_or) 
+
+
+#not / lo opuesto
+respuesta_not = not(500 == 500 or 200 != 201 or 80 > 100)
+print(respuesta_not)
+
